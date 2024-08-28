8af30364d7451dcfbe324ceb798986be3f45024f
Machado, Leonardo <leonardo.machado@kaefer.com>
2024-08-28 17:30:06 -0300

    Refactor: Alterando para uso em Channel e Parallel foreach

--- src/etl/sql/SqlServerCall.cs
+++ src/etl/sql/SqlServerCall.cs
@@ -14,7 +14,7 @@ public class SqlServerCall(string conStr) : IDisposable
                                     string sysName,
                                     string? database = null)
     {
-        _connection.Open();
+        GetConnection();
         
         if (database != null) 
         {
@@ -26,11 +26,17 @@ public class SqlServerCall(string conStr) : IDisposable
             BulkCopyTimeout = 1000,
             DestinationTableName = $"{sysName.ToUpper()}.{tableName.ToUpper()}"
         };
-
-        await bulkCopy.WriteToServerAsync(table);
-        Log.Out($"API return has been written into the table: {sysName.ToUpper()}.{tableName.ToUpper()}");
-        _connection.Close();
-        return ReturnedValues.MethodSuccess;
+            
+        try {
+            await bulkCopy.WriteToServerAsync(table);
+            Log.Out($"API return has been written into the table: {sysName.ToUpper()}.{tableName.ToUpper()}");
+            return Constants.MethodSuccess;
+        } catch (SqlException ex) {
+            Log.Out($"Error while attempting insert: {ex.Message}");
+            return Constants.MethodFail;
+        } finally {
+            _connection.Close();
+        }
     }
 
     public async Task CreateTable(DataTable table,
@@ -40,8 +46,10 @@ public class SqlServerCall(string conStr) : IDisposable
     {
         if (!IsCreated(tableName, database))
         {
+            GetConnection();
+            
             Log.Out($"Failed to find table {sysName.ToUpper()}.{tableName.ToUpper()}, proceeding with creation...");
-            using SqlCommand command = new("", _connection);
+            using SqlCommand cmd = new("", _connection);
 
             string createTableQuery = $"CREATE TABLE {sysName.ToUpper()}.{tableName.ToUpper()} (";
 
@@ -51,17 +59,106 @@ public class SqlServerCall(string conStr) : IDisposable
             }
             createTableQuery += $"ID_DW_{tableName.ToUpper()} INT NOT NULL IDENTITY(1,1) CONSTRAINT IX_{tableName.ToUpper()}_SK PRIMARY KEY, ";
             createTableQuery += $"DT_UPDATE_{tableName.ToUpper()} DATETIME NOT NULL CONSTRAINT CK_UPDATE_{tableName.ToUpper()} DEFAULT(GETDATE()));";
-            command.CommandText = createTableQuery;
-            await command.ExecuteNonQueryAsync();
+            cmd.CommandText = createTableQuery;
+            await cmd.ExecuteNonQueryAsync();
         } else {
             Log.Out($"Table {tableName.ToUpper()} already exists.");
         }
         _connection.Close();
     }
 
+    public DataTable GetTableFromServer(string database, SqlCommand cmd, string[]? options = null)
+    {
+        GetConnection();
+
+        if (database != null)
+        {
+            _connection.ChangeDatabase(database);
+        }      
+
+        DataTable data = new() {
+            TableName = options?[0] ?? "Table"
+        };
+
+        using SqlDataAdapter adapter = new(cmd);
+        adapter.Fill(data);
+
+        _connection.Close();
+
+        return data;
+    }
+
+    public void ExecuteCommand(SqlCommand cmd)
+    {
+        GetConnection();
+        cmd.ExecuteNonQuery();
+        _connection.Close();
+    }
+
+    public void CleanDestination(string sysName, string tableName, object[] options, string stmt)
+    {
+        GetConnection();
+
+        using SqlCommand cmd = new() {
+            Connection = _connection
+        };
+
+        int lineCount = GetLineCount(sysName, tableName);
+
+        switch (lineCount, options[0])
+        {
+            case (> 0, Constants.Incremental):
+                cmd.CommandText = stmt;
+                cmd.Parameters.AddWithValue("@NOMEIND", options[1]);
+                cmd.Parameters.AddWithValue("@TABELA", options[2]);
+                cmd.Parameters.AddWithValue("@VL_CORTE", options[3].ToString());
+                cmd.Parameters.AddWithValue("@COL_DT", options[4]);
+                cmd.ExecuteNonQuery();
+
+                Log.Out($"Table {tableName}");
+                break;
+            case (0, Constants.Incremental):
+                cmd.CommandText = $"TRUNCATE TABLE {sysName}.{tableName};";
+                cmd.ExecuteNonQuery();
+                break;            
+            case (_, Constants.Total):
+                cmd.CommandText = $"TRUNCATE TABLE {sysName}.{tableName};";
+                cmd.ExecuteNonQuery();
+                break;
+            default:
+                break;
+        }
+
+        cmd.ExecuteNonQuery();
+
+        _connection.Close();
+    }
+    private int GetLineCount(string sysName, string tableName)
+    {
+        GetConnection();
+
+        using SqlCommand cmd = new() {
+            CommandText = $"SELECT COUNT(1) FROM {sysName.ToUpper()}.{tableName.ToUpper()} WITH(NOLOCK);",
+            Connection = _connection
+        };
+
+        var ret = cmd.ExecuteScalar() ?? "0";
+
+        _connection.Close();
+        return int.Parse(cmd.ToString()!);
+    }
+
+    private void GetConnection()
+    {
+        if (_connection.State != ConnectionState.Open) {
+            _connection.Open();
+        }
+    }
+
     private bool IsCreated(string tableName, string? database = null)
     {
-        _connection.Open();
+        GetConnection();
+
         if (database != null) 
         {
             _connection.ChangeDatabase(database);
@@ -73,7 +170,9 @@ public class SqlServerCall(string conStr) : IDisposable
         };
 
         var ret = cmd.ExecuteScalar() ?? "0";
-        return int.Parse(ret.ToString()!) == ReturnedValues.MethodSuccess;
+        _connection.Close();
+
+        return int.Parse(ret.ToString()!) == Constants.MethodSuccess;
     }
 
     public void Dispose()
--- src/shared/Constants.cs
+++ src/shared/Constants.cs
@@ -1,6 +1,11 @@
 namespace DataConnect.Shared;
-
 public static class Constants
 {
+    public const int MethodSuccess = 1;
+    public const int MethodFail = 0;
+    public const int ProgramSucess = 0;
+    public const int ProgramFail = 1;
+    public const char Incremental = 'I';
+    public const char Total = 'T';
     public const string ProgramVersion = "1.0.0";
 }
