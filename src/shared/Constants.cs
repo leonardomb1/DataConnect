56feccc429a55ce83efb04eb0c2fb1ff78792f8c
Machado, Leonardo <leonardo.machado@kaefer.com>
2024-10-09 15:07:20 -0300

    Fix: Ajustando timeout comando de delete

--- src/shared/Constants.cs
+++ src/shared/Constants.cs
@@ -8,8 +8,10 @@ public static class Constants
     public const int LogBeginExecute = 5;
     public const int LogEndExecute = 6;
     public const int LogErrorExecute = 4;
+    public const int HourInSeconds = 3600;
     public const char Incremental = 'I';
     public const char Total = 'T';
     public const char Delete = 'D';
     public const string ProgramVersion = "1.0.2";
+    
 }
